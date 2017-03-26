6a147da2a0fba80959565931a008fbb35739baf7
Justin Gareau <jgareau@evernote.com>
2017-03-25 22:51:25 -0700

    Add button to access image picker

--- StockViewControllers/ViewController.swift
+++ StockViewControllers/ViewController.swift
@@ -15,11 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func experiment(_ sender: Any) {
+        let nextController = UIImagePickerController()
+        self.present(nextController, animated: true, completion: nil)
+    }
 
 }
 
